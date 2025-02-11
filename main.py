dbaa425aa752b5d0b43fb1ca0370659e17fccfa9
Saima Firdoush <85526675+saimafirdousfh@users.noreply.github.com>
2025-02-11 17:25:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import random
+
+
+
+
+
+def get_choices():
+  player_choice = input("Enter a choice -> rock,paper,scissors: ")
+  options = ["rock", "paper", "scissors"]
+  computer_choice = random.choice(options)
+  
+  choices = {"player":player_choice,"computer":computer_choice}
+  return choices
+
+#choices = get_choices()
+#print(choices)
+
+
+#food = ["pizza","carrots","eggs"] #list of strings
+#dinner = random.choice(food) #randomly selects from list
+
+def check_win(player,computer):
+  print(f"you chose {player}, computer chose {computer}")
+  if player == computer:
+    return "Its a TIE!"
+  elif player =="rock":
+    if computer=="scissors":
+        return "Rock smashes scissors! YOU WIN!🏆"
+    else:
+       return "Paper covers Rock YOU Lose🥹"
+  elif player =="paper":
+    if computer=="rock":
+       return "Paper  covers rock ! YOU WIN!🔥"
+    else:
+       return "Scissors cuts paper YOU Lose🥹"
+  
+  elif player == "scissors":
+    if computer=="paper":
+      return "Scissors cuts paper! YOU WIN!🏆"
+    else: 
+       return "Rock smashes scissors You Lose🥹"
+
+
+
+
+
+
+
+choices = get_choices()
+
+result = check_win(choices["player"],choices["computer"])
+print(result)
